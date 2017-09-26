1860f6ed324dafd9dfd501670e90c82ccc41e2d8
Immanuel Thomas <immanuel278@gmail.com>
2017-09-25 23:06:24 -0700

    Created Scanner activity with camera preview
    Main activity now creates scanner activity and can receive code. Scanner
    activity also displays preview from Camera (1.0) with weird preview size

--- app/src/main/java/com/immanuel/homeinventory/MainActivity.java
+++ app/src/main/java/com/immanuel/homeinventory/MainActivity.java
@@ -1,7 +1,11 @@
 package com.immanuel.homeinventory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startScanner(View view) {
+        Intent intent = new Intent(this, ScannerActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK) {
+                // Get the Intent that started this activity and extract the string
+                String message = data.getStringExtra(ScannerActivity.CODE_KEY);
+
+                // Capture the layout's TextView and set the string as its text
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(message);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/immanuel/homeinventory/ScannerActivity.java
@@ -0,0 +1,100 @@
+package com.immanuel.homeinventory;
+
+import android.content.Intent;
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.EditText;
+
+@SuppressWarnings("deprecation")
+public class ScannerActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    public static final String CODE_KEY = "com.immanuel.homeinventory.CODE";
+
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+
+    public void returnCode(View view) {
+        Intent intent = new Intent();
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        String message = editText.getText().toString();
+        intent.putExtra(CODE_KEY, message);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView2);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera = Camera.open();
+        } catch (RuntimeException e) {
+            System.err.println(e);
+            return;
+        }
+        Camera.Parameters param;
+        param = camera.getParameters();
+
+        // TODO: check preview size correctly
+        param.setPreviewSize(352, 288);
+        camera.setParameters(param);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+            System.err.println(e);
+            return;
+        }
+    }
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        refreshCamera();
+    }
+
+    public void refreshCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            return;
+        }
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+            System.err.println(e);
+            return;
+        }
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+            System.err.println(e);
+            return;
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // stop preview and release camera
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+}
